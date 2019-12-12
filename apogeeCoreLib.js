b6cdb430cd42933f612a0e5daf98dbc31d0103cb
sutter-dave <sutter@intransix.com>
2019-12-11 17:02:10 -0800

    making libraries for core and app. I need to fix deployment for these still

---
+++ apogeeCoreLib.js
@@ -0,0 +1,9 @@
+//This module exports the public interface to the Apogee Core Library
+//(It also loads the needed empty imports)
+export {default as Workspace} from "/apogee/data/Workspace.js";
+export { doAction } from "/apogee/actions/action.js";
+export { validateTableName } from "/apogee/lib/codeCompiler.js";
+export {default as Messenger} from "/apogee/actions/Messenger.js";
+import "/apogee/commandConfig.js";
+import "/apogee/tableConfig.js";
+import "/debug/debugHook.js";
