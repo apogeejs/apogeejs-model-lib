82dd6525430e34362802fe0b679f01b7e51661b8
sutter-dave <sutter@intransix.com>
2021-02-10 18:26:36 -0800

    work in progress on build system

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,8 +1,8 @@
-const { src, dest, series, parallel, task } = require('gulp');
-const releasePackageJson = require("../package.json");
+const {series, parallel, src, dest} = require('gulp');
 const rollup = require('rollup');
-const createResolveIdPlugins = require("./resolveIdPlugin.js");
-const fs = require("fs");
+const {getJsFileHeader, getReleaseFolder, makeSureReleaseNotPresent, createResolveId} = require("../../apogeejs-admin/build-src/build-utils.js");
+
+const releasePackageJson = require("../package.json");
 
 //read data from the release package.json
 let isDevRelease = releasePackageJson.DEV_RELEASE;
@@ -10,54 +10,18 @@ let version = releasePackageJson.version;
 let repoName = releasePackageJson.name;
 let esModuleFileName = releasePackageJson.module;
 let npmModuleFileName = releasePackageJson.main;
-let dependencies = releasePackageJson.dependencies;
-let externalModuleMap = releasePackageJson.config.externalModuleMap ? releasePackageJson.config.externalModuleMap : {};  //add safety checks
-let localPathAlias = releasePackageJson.config.localPathAlias;
-
-if(!localPathAlias) throw new Error("Local path alias missing!");
 
 //for absolute references
 const PATH_TO_ABSOLUTE_ROOT = "../..";
-let {esResolveId,npmResolveId}  = createResolveIdPlugins(__dirname,PATH_TO_ABSOLUTE_ROOT,repoName,localPathAlias,externalModuleMap);
+let resolveId = createResolveId(__dirname,PATH_TO_ABSOLUTE_ROOT);
 
-//IMPORT THIS!!!
-
-//version header for js files
-function getJsFileHeader(fileName) {
-    return "// File: " + fileName + "\n" +
-        "// Version: " + version + "\n" +
-        "// Copyright (c) 2016-2021 Dave Sutter\n" + 
-        "// License: MIT\n";
-}
-
-//IMPORT THIS!!!
-
-//==============================
-// Top Level Values
-//==============================
-const RELEASE_PARENT_FOLDER = "../../apogeejs-admin";
-const RELEASE_FOLDER = isDevRelease ?  "releases-dev" : "releases";
-const RELEASE_NAME = "v" + version;
-const OUTPUT_FOLDER = RELEASE_PARENT_FOLDER + "/" + RELEASE_FOLDER + "/" + repoName + "/" + RELEASE_NAME;
-
-
-//IMPORT THIS!!!
-//const RELEASE_FOLDER = getReleaseFolder(versionConfig);
+const outputFolder = getReleaseFolder(repoName,version,isDevRelease);
 
 
 //======================================
 // Release Info
 //======================================
 
-//IMPORT THIS!!!
-function makeSureReleaseNotPresent() {
-    return new Promise( (resolve,reject) => {
-        fs.stat(OUTPUT_FOLDER, (err, stats) => {
-            if (err) resolve("File is not present!");
-            else reject("Release is already present! If this should not be true, check the version numbers");
-        });
-    })
-}
 
 //base files - version info
 const BASE_FILES = [
@@ -65,78 +29,64 @@ const BASE_FILES = [
 ]
 
 let copyReleaseInfoTask = parallel(
-    () => copyFilesTask(BASE_FILES,OUTPUT_FOLDER)
+    () => copyFilesTask(BASE_FILES,outputFolder)
 )
 
-//==============================
-// Web App
-//==============================
-
-let genBunTest = (options,bundle,isWrite) => {
-    for(let fileName in bundle) {
-        console.log("Bundle fileName: " + fileName);
-        let content = bundle[fileName];
-        if(content.type == "chunk") {
-            if(content.imports) {
-                console.log("Imports: " + content.imports);
-                content.imports = content.imports.map(importName => "/magicpath/" + importName);
-                console.log("Imports 2: " + content.imports);
-            }
-        }
-    }
-}
-
-
-
-function packageEsLibTask() {
-    return rollup.rollup({
-        input: '../src/' + esModuleFileName,
-        plugins: [
-            {resolveId: esResolveId, generateBundle: genBunTest}
-        ]
-    }).then(bundle => {
-        return Promise.all([
-            bundle.write(
-                { 
-                    file: OUTPUT_FOLDER + "/" + esModuleFileName,
-                    format: 'es',
-                    banner: getJsFileHeader(esModuleFileName)
-                }
-            )
-        ]);
-    });
-}
-
-function packageNpmLibTask() {
-    return rollup.rollup({
-        input: '../src/' + esModuleFileName,
-        plugins: [
-            {resolveId: npmResolveId}
-        ]
-    }).then(bundle => {
-        return Promise.all([
-            bundle.write(
-                { 
-                    file: OUTPUT_FOLDER + "/" + npmModuleFileName,
-                    format: 'cjs',
-                    banner: getJsFileHeader(npmModuleFileName)
-                }
-            )
-        ]);
-    });
-}
-
-//==============================
-// Utility Tasks
-//==============================
-
-//copy files utility function
-//IMPORT THIS!!!
+/** This function is a gulp task that copies files to a destination folder. */
 function copyFilesTask(fileList,destFolder) {
     return src(fileList,{allowEmpty: true})
         .pipe(dest(destFolder));
 }
 
+
+//==============================
+// Library Module
+//==============================
+
+function packageLibTask() {
+    return rollup.rollup({
+        input: '../src/' + esModuleFileName,
+        external: [
+            "/apogeejs-base-lib/src/apogeeBaseLib.js",
+            "/apogeejs-util-lib/src/apogeeUtilLib.js",
+            "/apogeejs-admin/ext/esprima/esprima_2.7.3/esprima.es.js"
+		],
+        plugins: [
+            {resolveId}
+        ]
+    }).then(bundle => {
+        return Promise.all([
+            bundle.write(
+                { 
+                    file: outputFolder + "/" + esModuleFileName,
+                    format: 'es',
+                    banner: getJsFileHeader(esModuleFileName,version),
+                    paths: {
+                        "/apogeejs-base-lib/src/apogeeBaseLib.js": "/apogeejs-base-lib/src/apogeeBaseLib.js",
+                        "/apogeejs-util-lib/src/apogeeUtilLib.js": "/apogeejs-util-lib/src/apogeeUtilLib.js",
+                        "/apogeejs-admin/ext/esprima/esprima_2.7.3/esprima.es.js": "/apogeejs-admin/ext/esprima/esprima_2.7.3/esprima.es.js"
+
+                        // "/apogeejs-base-lib/src/apogeeBaseLib.js": "/apogeejs-admin/releases/apogeejs-base-lib/v2.0.0/apogeeBaseLib.js",
+                        // "/apogeejs-util-lib/src/apogeeUtilLib.js": "/apogeejs-admin/releases/apogeejs-util-lib/v2.0.0/apogeeUtilLib.js",
+                        // "/apogeejs-admin/ext/esprima/esprima_2.7.3/esprima.es.js": "/apogeejs-admin/ext/esprima/esprima_2.7.3/esprima.es.js"
+                    }
+                }
+            ),
+            bundle.write(
+                { 
+                    file: outputFolder + "/" + npmModuleFileName,
+                    format: 'cjs',
+                    banner: getJsFileHeader(npmModuleFileName,version),
+                    paths: {
+                        "/apogeejs-base-lib/src/apogeeBaseLib.js": "apogeejs-base-lib",
+                        "/apogeejs-util-lib/src/apogeeUtilLib.js": "apogeejs-util-lib",
+                        "/apogeejs-admin/ext/esprima/esprima_2.7.3/esprima.es.js": "esprima"
+                    }
+                }
+            )
+        ]);
+    });
+}
 
 //============================
 // Exports
@@ -144,10 +94,9 @@ function copyFilesTask(fileList,destFolder) {
 
 //This task executes the complete release
 exports.release = series(
-    makeSureReleaseNotPresent,
+    () => makeSureReleaseNotPresent(outputFolder),
     parallel(
         copyReleaseInfoTask,
-        packageEsLibTask,
-        packageNpmLibTask
+        packageLibTask
     )
 );
